6a759cad8c07acb6916c64dbbfb2a33b15d28bc1
Meghna-24 <meghnamadhu24@gmail.com>
2025-06-21 23:04:04 +0530

    Update app.py

--- server/app.py
+++ server/app.py
@@ -114,3 +114,17 @@ def get_leaderboard():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+from flask import Flask, request, jsonify, send_from_directory
+import os
+
+app = Flask(__name__, static_folder='../client', static_url_path='')
+
+@app.route('/')
+def serve_index():
+    return send_from_directory(app.static_folder, 'index.html')
+
+@app.route('/<path:path>')
+def serve_static(path):
+    return send_from_directory(app.static_folder, path)
